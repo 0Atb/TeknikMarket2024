1f180e9befddc48fb03038509123c63aaeb12d29
Ahmet <aeldra995@hotmail.com>
2024-01-14 17:19:52 +0300

    #DataAccessEfRepository

---
+++ TeknikMarket.DataAccess/Concrete/EntityFramework/Repository/EfUserRoleRepository.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.Data.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeknikMarket.DataAccess.Absract;
+using TeknikMarket.DataAccess.Concrete.EntityFramework.Context;
+using TeknikMarket.Model.Entity;
+
+namespace TeknikMarket.DataAccess.Concrete.EntityFramework.Repository
+{
+    public class EfUserRoleRepository : EfRepositoryBase<UserRole, TeknikMarketContext>, IUserRoleRepository
+    {
+    }
+}
